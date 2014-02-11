637d81ea91d683fb85b745316cdb8c8f2259dd5f
ludawei <ludawei@platomix.net>
2014-02-11 12:36:52 +0800

    Initial Commit

---
+++ QQNewsViewSwitch/TSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TSAppDelegate.h
+//  QQNewsViewSwitch
+//
+//  Created by ludawei on 14-2-11.
+//  Copyright (c) 2014年 platomix.dw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ QQNewsViewSwitch/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  QQNewsViewSwitch
+//
+//  Created by ludawei on 14-2-11.
+//  Copyright (c) 2014年 platomix.dw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TSAppDelegate class]));
+    }
+}
